86ef69cf8a4899670f239ddbdc80a0cb0e5d16a9
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-09-04 21:50:53 +1000

    getApi function added within submitButton function, returning correct data to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,21 +16,38 @@ submitButton.addEventListener("click", function (event) {
     //render name of location searched on screen
     document.querySelector("#location").textContent = nameOfLocation;
 
+    function getApi() {
+        var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + nameOfLocation + '&appid=df6dd1cd15022b8002c172b01cde8380&units=metric';
+
+        fetch(requestUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+            });
+    }
+
+    getApi();
+
+
 
 });
 
 // function created to see what data is returned from API
 
-function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=df6dd1cd15022b8002c172b01cde8380&units=metric'
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-        });
-}
 
-getApi();
+
+// var getWeather = function(city = nameOfLocation) {
+//     var apiUrl =  'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=df6dd1cd15022b8002c172b01cde8380&units=metric';
+//     fetch(apiUrl)
+//     .then(function (response) {
+//         if(response.ok) {
+//             response.json().then(function (data) {
+//                 console.log(data);
+
+//             });
+//         }
+//     })
+// }
