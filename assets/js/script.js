846792251a6c68b6701a88e4aadbab4babd4263e
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-09-04 19:56:33 +1000

    getApi function created to test data fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,24 @@ submitButton.addEventListener("click", function (event) {
     localStorage.setItem("nameOfLocation", JSON.stringify(nameOfLocation));
     console.log(nameOfLocation);
 
-    //display name of location searched on screen
+    //render name of location searched on screen
     document.querySelector("#location").textContent = nameOfLocation;
+
+
 });
 
 
 
+function getApi() {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=tumut&appid=df6dd1cd15022b8002c172b01cde8380'
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        });
+}
+
+getApi();
