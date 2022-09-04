5386e96784af65a57bd13214ec2f5bcbacd6da9b
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-09-04 15:49:22 +1000

    var submitEl, locationName created. submitResponse function created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-s
+var submitEl = document.querySelector("#submit");
+var locationName = document.querySelector("#location-name");
+
+function submitResponse(event) {
+    event.preventDefault();
+    document.querySelector("#location").textContent = locationName;
+
+
+}
+
+// Add event listener to submit search location
+submitEl.addEventListener("click", submitResponse);
