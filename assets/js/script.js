d09388886d6da2292615da654bf27db8338c4e73
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-09-22 01:03:41 +1000

    changes made to js with one on one tutor, HTML changes made, API fetch working and rendering card dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 var locationName = document.querySelector("#location-name");
 var submitButton = document.querySelector("#submit");
+var todayContainer = document.getElementById('today');
+
+var apiKey = "d91f911bcf2c0f925fb6535547a5ddc9";
+var api = "https://api.openweathermap.org";
+
+// Add timezone plugins to day.js
+
+dayjs.extend(window.dayjs_plugin_utc);
+
+dayjs.extend(window.dayjs_plugin_timezone);
 
 
 
@@ -18,9 +28,6 @@ submitButton.addEventListener("click", function (event) {
     localStorage.setItem("nameOfLocation", JSON.stringify(nameOfLocation));
     console.log(nameOfLocation);
 
-    //render name of location searched on screen and capitalise first letter
-    // document.querySelector("#location").textContent = nameOfLocation[0].toUpperCase() + nameOfLocation.substring(1);
-
 
     getCity(nameOfLocation);
 
@@ -29,7 +36,7 @@ submitButton.addEventListener("click", function (event) {
 });
 
 function getCity(nameOfLocation) {
-    var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${nameOfLocation}&limit=5&appid=df6dd1cd15022b8002c172b01cde8380`;
+    var requestUrl = `${api}/geo/1.0/direct?q=${nameOfLocation}&limit=5&appid=${apiKey}`;
     console.log(requestUrl);
     fetch(requestUrl)
         .then(function (response) {
@@ -41,6 +48,7 @@ function getCity(nameOfLocation) {
             } else {
                 console.log(data)
                 getWeather(data[0])
+
             }
 
         }).catch(function (err) {
@@ -51,20 +59,21 @@ function getCity(nameOfLocation) {
 }
 
 function getWeather(location) {
-    var { lat, lon } = location
-    var city = location.name
-    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=d91f911bcf2c0f925fb6535547a5ddc9`;
-
+    var { lat, lon } = location;
+    var city = location.name;
+    console.log(location);
+    var apiUrl = `${api}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=${apiKey}`;
     fetch(apiUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            sharedData(city, data)
-            console.log(data)
-        }).catch(function (err) {
-
-            console.error(err);
+            sharedData(city, data);
+            console.log(data);
+            // }).catch(function (err) {
+            //     // console.log(getWeather);
+            //     // console.log(data);
+            //     console.error(err);
         });
 
 }
@@ -74,9 +83,13 @@ function sharedData(city, data) {
     forecastWeather(data.daily, data.timezone);
 }
 function currentWeather(city, weather, timezone) {
-    var date = dayjs().tz(timezone).format('M/D/YYYY');
+    var date = dayjs().tz(timezone).format('D/M/YYYY');
 
     var tempC = weather.temp;
+    var humidity = weather.humidity;
+    var windMph = weather.wind_speed;
+    var uvi = weather.uvi;
+
     //pullotherdata
     var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
     var iconDescription = weather.weather[0].description || weather[0].main;
@@ -89,16 +102,24 @@ function currentWeather(city, weather, timezone) {
     var humidityEl = document.createElement('p')
     var uvEl = document.createElement('p')
     var uvIBadge = document.createElement('button')
-
+    var weatherImage = document.createElement('img')
     card.setAttribute('class', 'card')
     cardBody.setAttribute('class', 'card-body')
     card.append(cardBody)
 
     tempCEl.setAttribute('class', "card-text")
+    windEl.setAttribute('class', "card-text")
+    humidityEl.setAttribute('class', "card-text")
+    weatherImage.setAttribute('src', iconUrl)
+    weatherImage.setAttribute('alt', iconDescription)
+    weatherImage.setAttribute('class', "weather-img")
+    tempCEl.textContent = `current temp: ${tempC} °C`;
+    humidityEl.textContent = `humidity: ${humidity}%`;
+    windEl.textContent = `wind: ${windMph} km/h`;
     //add attributes for other elements
 
     cardTitle.textContent = `${city} (${date})`;
-
+    cardTitle.append(weatherImage);
     cardBody.append(cardTitle, tempCEl, windEl, humidityEl)
 
     uvEl.textContent = 'uv index: ';
@@ -114,7 +135,7 @@ function currentWeather(city, weather, timezone) {
     uvIBadge.textContent = uvi;
     uvEl.append(uvIBadge);
     cardBody.append(uvEl);
-    var todayContainer = document.querySelector('.today');
+    // todayContainer.innerHTML = "";
     todayContainer.append(card);
 }
 // forecastWeather to create, midnight to midgnight start to end date
