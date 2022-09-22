f65b99837bb9fb5e5e851c7591e30dc2118b0922
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-09-22 14:06:36 +1000

    forecast function 5 days added to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 var locationName = document.querySelector("#location-name");
 var submitButton = document.querySelector("#submit");
 var todayContainer = document.getElementById('today');
+var forecastContainer = document.getElementById('forecast');
 
-var apiKey = "d91f911bcf2c0f925fb6535547a5ddc9";
+var apiKey = "2a2c6d77334d26a9c34ac000ab9a9fe8";
 var api = "https://api.openweathermap.org";
 
 // Add timezone plugins to day.js
@@ -35,6 +36,7 @@ submitButton.addEventListener("click", function (event) {
     locationName.value = "";
 });
 
+// function to obtain lat and long of search location
 function getCity(nameOfLocation) {
     var requestUrl = `${api}/geo/1.0/direct?q=${nameOfLocation}&limit=5&appid=${apiKey}`;
     console.log(requestUrl);
@@ -62,7 +64,8 @@ function getWeather(location) {
     var { lat, lon } = location;
     var city = location.name;
     console.log(location);
-    var apiUrl = `${api}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=${apiKey}`;
+    var apiUrl = `${api}/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=${apiKey}`;
+    console.log(apiUrl);
     fetch(apiUrl)
         .then(function (response) {
             return response.json();
@@ -70,27 +73,29 @@ function getWeather(location) {
         .then(function (data) {
             sharedData(city, data);
             console.log(data);
-            // }).catch(function (err) {
-            //     // console.log(getWeather);
-            //     // console.log(data);
-            //     console.error(err);
+        }).catch(function (err) {
+
+            console.error(err);
         });
 
 }
 
+//function to share data between current and forecast weather
 function sharedData(city, data) {
     currentWeather(city, data.current, data.timezone);
     forecastWeather(data.daily, data.timezone);
 }
+
+
 function currentWeather(city, weather, timezone) {
     var date = dayjs().tz(timezone).format('D/M/YYYY');
 
     var tempC = weather.temp;
     var humidity = weather.humidity;
-    var windMph = weather.wind_speed;
+    var windKph = weather.wind_speed;
     var uvi = weather.uvi;
 
-    //pullotherdata
+    // get weather icons
     var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
     var iconDescription = weather.weather[0].description || weather[0].main;
 
@@ -115,12 +120,15 @@ function currentWeather(city, weather, timezone) {
     weatherImage.setAttribute('class', "weather-img")
     tempCEl.textContent = `current temp: ${tempC} °C`;
     humidityEl.textContent = `humidity: ${humidity}%`;
-    windEl.textContent = `wind: ${windMph} km/h`;
+    windEl.textContent = `wind: ${windKph} km/h`;
+
     //add attributes for other elements
 
     cardTitle.textContent = `${city} (${date})`;
     cardTitle.append(weatherImage);
     cardBody.append(cardTitle, tempCEl, windEl, humidityEl)
+
+    // uv index colour change according to uv level
 
     uvEl.textContent = 'uv index: ';
     uvIBadge.classList.add('btn', 'btn-sm');
@@ -135,8 +143,68 @@ function currentWeather(city, weather, timezone) {
     uvIBadge.textContent = uvi;
     uvEl.append(uvIBadge);
     cardBody.append(uvEl);
-    // todayContainer.innerHTML = "";
+    todayContainer.innerHTML = "";
     todayContainer.append(card);
 }
 // forecastWeather to create, midnight to midgnight start to end date
-// var for start and var for end for loop on daily forecast array, create function called forcast card and call out in for loop
+// var for start and var for end for loop on daily forecast array, create function called forecast card and call out in for loop
+
+function forecastWeather(daily, timezone) {
+    // create time zone for forecast
+    var startDay = dayjs().tz(timezone).add(1, "day").startOf("day").unix();
+    var endDay = dayjs().tz(timezone).add(6, "day").startOf("day").unix();
+
+    var forecastCol = document.createElement('div');
+    var heading = document.createElement('h4');
+
+    forecastCol.setAttribute('class', 'col-12');
+    heading.classList.add('row-white');
+    heading.textContent = '5 day forecast';
+    forecastCol.append(heading);
+
+
+    forecastContainer.innerHTML = "";
+
+    forecastContainer.append(forecastCol);
+    for (let i = 0; i < daily.length; i++) {
+        if (daily[i].dt >= startDay && daily[i].dt < endDay) {
+            forecastCard(daily[i], timezone);
+        }
+    }
+}
+
+function forecastCard(forecast, timeZone) {
+    var unixTs = forecast.dt;
+    var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+    var iconDescription = forecast.weather[0].description;
+    var tempc = forecast.temp.day;
+    var { humidity } = forecast;
+    var windkph = forecast.wind_speed;
+    var col = document.createElement("div");
+    var card = document.createElement("div");
+    var cardBody = document.createElement("div");
+    var cardtitle = document.createElement("h5");
+    var weathericon = document.createElement("img");
+    var tempEl = document.createElement("p");
+    var windEl = document.createElement("p");
+    var humidityEl = document.createElement("p");
+    col.append(card);
+    card.append(cardBody);
+    cardBody.append(cardtitle, weathericon, tempEl, windEl, humidityEl);
+    col.setAttribute("class", "col-md");
+    col.classList.add("5-day-card");
+    card.setAttribute("class", "card bg-primary h-100 text-white");
+    cardBody.setAttribute("class", "card-body p-2");
+    cardtitle.setAttribute("class", "card-title");
+    tempEl.setAttribute("class", "card-text");
+    windEl.setAttribute("class", "card-text");
+    humidityEl.setAttribute("class", "card-text");
+
+    cardtitle.textContent = dayjs.unix(unixTs).tz(timeZone).format("ddd D MMM");
+    weathericon.setAttribute("src", iconUrl);
+    weathericon.setAttribute("alt", iconDescription);
+    windEl.textContent = `Wind: ${windkph} km/h`;
+    humidityEl.textContent = `Humidity: ${humidity}%`;
+    tempEl.textContent = `Temp: ${tempc}°C`
+    forecastContainer.append(col);
+}
